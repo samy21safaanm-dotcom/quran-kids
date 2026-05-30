4b489a0d51faf1d8804fac1920ade06d04cee93f
samy21safaanm-dotcom <samy.21.safaan.m@gmail.com>
2026-05-30 18:33:27 +0300

    Remove node_modules from repository and update .gitignore

--- node_modules/@aws-sdk/client-transcribe/dist-types/pagination/ListLanguageModelsPaginator.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Paginator } from "@smithy/types";
-import { ListLanguageModelsCommandInput, ListLanguageModelsCommandOutput } from "../commands/ListLanguageModelsCommand";
-import type { TranscribePaginationConfiguration } from "./Interfaces";
-/**
- * @public
- */
-export declare const paginateListLanguageModels: (config: TranscribePaginationConfiguration, input: ListLanguageModelsCommandInput, ...rest: any[]) => Paginator<ListLanguageModelsCommandOutput>;
